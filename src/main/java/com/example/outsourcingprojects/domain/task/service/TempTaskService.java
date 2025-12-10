ab97d24284d0cf48ba3da3f62f0fa2f6ef9b2b31
d-perado <sjm01065@naver.com>
2025-12-10 12:03:42 +0900

    feat: 대시보드 통계 조회

--- src/main/java/com/example/outsourcingprojects/domain/task/service/TempTaskService.java
+++ src/main/java/com/example/outsourcingprojects/domain/task/service/TempTaskService.java
@@ -1,9 +1,13 @@
 package com.example.outsourcingprojects.domain.task.service;
 
+import com.example.outsourcingprojects.common.entity.QTask;
 import com.example.outsourcingprojects.common.entity.Task;
+import com.example.outsourcingprojects.common.model.TaskStatusType;
 import com.example.outsourcingprojects.domain.task.repository.TempTaskRepository;
+import com.example.outsourcingprojects.domain.task.tempDto.DashBoardDTO;
 import com.example.outsourcingprojects.domain.task.tempDto.GetTaskSummaryResponse;
 import com.example.outsourcingprojects.domain.task.tempDto.TaskSummaryDTO;
+import com.querydsl.core.Tuple;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -16,15 +20,11 @@ public class TempTaskService {
 
     private final TempTaskRepository tempTaskRepository;
 
-    public GetTaskSummaryResponse GetTaskSummaries(Long userId) {
+    public GetTaskSummaryResponse getTaskSummaries(Long userId) {
 
-        final Long UPCOMING_STATUS = 1L;
-        final Long TODAY_STATUS = 2L;
-        final Long OVERDUE_STATUS = 3L;
-
-        Page<Task> upcomingTasks = tempTaskRepository.findAllByAssigneeIdAndStatus(userId, UPCOMING_STATUS);
-        Page<Task> todayTasks = tempTaskRepository.findAllByAssigneeIdAndStatus(userId, TODAY_STATUS);
-        Page<Task> overdueTasks = tempTaskRepository.findAllByAssigneeIdAndStatus(userId, OVERDUE_STATUS);
+        Page<Task> upcomingTasks = tempTaskRepository.findAllByAssigneeIdAndStatus(userId, TaskStatusType.TODO.getStatusNum());
+        Page<Task> todayTasks = tempTaskRepository.findAllByAssigneeIdAndStatus(userId, TaskStatusType.IN_PROGRESS.getStatusNum());
+        Page<Task> overdueTasks = tempTaskRepository.findAllByAssigneeIdAndStatus(userId, TaskStatusType.DONE.getStatusNum());
 
         List<TaskSummaryDTO> upcomingSummaryTasks = upcomingTasks.stream().map(TaskSummaryDTO::from).toList();
         List<TaskSummaryDTO> todaySummaryTasks = todayTasks.stream().map(TaskSummaryDTO::from).toList();
@@ -33,4 +33,43 @@ public class TempTaskService {
         return GetTaskSummaryResponse.from(upcomingSummaryTasks, todaySummaryTasks, overdueSummaryTasks);
 
     }
+
+    public DashBoardDTO getDashBoard(Long userId) {
+        List<Tuple> statusTask = tempTaskRepository.countTasksByStatus();
+
+        //Todo(10L) > InProgress(20L) > Done(30L)
+        Long todoTasks = 0L;
+        Long inProgressTasks = 0L;
+        Long completedTasks = 0L;
+
+        for (Tuple tuple : statusTask) {
+            Long status = tuple.get(QTask.task.status);
+            Long count = tuple.get(QTask.task.id.count());
+
+            if (status == null || status == 0L) {
+                continue;
+            }
+            if (status == TaskStatusType.TODO.getStatusNum()) {
+                todoTasks = (count != null) ? count : 0L;
+            }
+            if (status == TaskStatusType.IN_PROGRESS.getStatusNum()) {
+                inProgressTasks = (count != null) ? count : 0L;
+            }
+            if (status == TaskStatusType.DONE.getStatusNum()) {
+                completedTasks = (count != null) ? count : 0L;
+            }
+        }
+        Long totalTasks = todoTasks + inProgressTasks + completedTasks;
+
+        Long overdueTasks = tempTaskRepository.countOverdueTask();
+
+        Long myTasksToday = tempTaskRepository.countMyTaskToday(userId);
+
+        Double teamProgress = ((completedTasks.doubleValue() + inProgressTasks.doubleValue()) / totalTasks.doubleValue()) * 100.0;
+
+        Double completionRate = completedTasks.doubleValue() / totalTasks.doubleValue() * 100.0;
+
+        return new DashBoardDTO(totalTasks, completedTasks, inProgressTasks, todoTasks, overdueTasks, teamProgress, myTasksToday, completionRate);
+    }
+
 }
