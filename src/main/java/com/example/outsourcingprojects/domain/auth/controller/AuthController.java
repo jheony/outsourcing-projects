31e6f6c2fa6d8cf10f172177b700f0a9d26880b4
윤지현 <139779783+jheony@users.noreply.github.com>
2025-12-10 15:43:02 +0900

    feat: 비밀번호 검증
    - VerifyPasswordRequest, VerifyPasswordResponse 생성

--- src/main/java/com/example/outsourcingprojects/domain/auth/controller/AuthController.java
+++ src/main/java/com/example/outsourcingprojects/domain/auth/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.example.outsourcingprojects.domain.auth.controller;
 
 import com.example.outsourcingprojects.common.util.response.GlobalResponse;
-import com.example.outsourcingprojects.domain.auth.service.AuthService;
 import com.example.outsourcingprojects.domain.auth.dto.request.LoginRequest;
+import com.example.outsourcingprojects.domain.auth.dto.request.VerifyPasswordRequest;
 import com.example.outsourcingprojects.domain.auth.dto.response.LoginResponse;
+import com.example.outsourcingprojects.domain.auth.dto.response.VerifyPasswordResponse;
+import com.example.outsourcingprojects.domain.auth.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,5 +26,13 @@ public class AuthController {
         LoginResponse response = authService.login(request);
 
         return GlobalResponse.success("로그인 성공", response);
+    }
+
+    @PostMapping("/verify-password")
+    public GlobalResponse<VerifyPasswordResponse> verifyPassword(@Valid @RequestBody VerifyPasswordRequest request) {
+
+        VerifyPasswordResponse response = authService.verifyPasswordHandler(request);
+
+        return GlobalResponse.success("비밀번호가 확인되었습니다.", response);
     }
 }
