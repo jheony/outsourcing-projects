a5903b5940ae81be96b6bc3a1d8a44e8f3a1d095
윤지현 <139779783+jheony@users.noreply.github.com>
2025-12-11 14:47:06 +0900

    feat: ActivityLog Entity 생성

--- src/main/java/com/example/outsourcingprojects/common/entity/ActivityLog.java
+++ src/main/java/com/example/outsourcingprojects/common/entity/ActivityLog.java
@@ -1,12 +1,37 @@
 package com.example.outsourcingprojects.common.entity;
 
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
-@Table(name = "activitylogs")
+@Table(name = "activities")
 public class ActivityLog {
-    //어떻게 구현할건지 고민해봐야 할 사안입니다.
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private User user;
+
+    @Column(name = "task_id")
+    private Long taskId;
+
+    @Column(name = "comment_id")
+    private Long commentId;
+
+    @Column(nullable = false)
+    private Long type;
+
+    @Column(nullable = false)
+    private String description;
+
+    @CreatedDate
+    private LocalDateTime timestamp;
 }
--- src/main/java/com/example/outsourcingprojects/common/model/ActivityType.java
+++ src/main/java/com/example/outsourcingprojects/common/model/ActivityType.java
@@ -7,15 +7,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ActivityType {
 
-    TASK_CREATED(101L),
-    TASK_UPDATED(102L),
-    TASK_DELETED(103L),
-    TASK_STATUS_CHANGED(104L),
-    COMMENT_CREATED(201L),
-    COMMENT_UPDATED(202L),
-    COMMENT_DELETED(203L);
+    TASK_CREATED(101L, "TASK", "작업 생성"),
+    TASK_UPDATED(102L, "TASK", "작업 수정"),
+    TASK_DELETED(103L, "TASK", "작업 삭제"),
+    TASK_STATUS_CHANGED(104L, "TASK", "작업 상태 변경"),
+    COMMENT_CREATED(201L, "COMMENT", "댓글 생성"),
+    COMMENT_UPDATED(202L, "COMMENT", "댓글 수정"),
+    COMMENT_DELETED(203L, "COMMENT", "댓글 삭제");
 
     private final long activityNum;
+    private final String targetType;
+    private final String action;
 
     public static ActivityType toType(Long value) throws Exception {
         if (value == null) throw new Exception();
