7970949e2081c9718e1749302632bcf5aa2bd6a3
whdal <dbswhdals2002z@gmail.com>
2025-12-12 00:30:58 +0900

    refactor: 코드 수정

--- src/main/java/com/example/outsourcingprojects/domain/task/dto/CreateTaskRequestDto.java
+++ src/main/java/com/example/outsourcingprojects/domain/task/dto/CreateTaskRequestDto.java
@@ -1,8 +1,6 @@
 package com.example.outsourcingprojects.domain.task.dto;
 
 
-import com.example.outsourcingprojects.common.model.PriorityType;
-import com.example.outsourcingprojects.common.model.TaskStatusType;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
