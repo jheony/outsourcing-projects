f2b64582abcbc73594f03ae753c6453ccdf807da
윤지현 <139779783+jheony@users.noreply.github.com>
2025-12-11 21:34:08 +0900

    feat: AoP 로깅 구현

--- src/main/java/com/example/outsourcingprojects/common/config/WebMvcConfig.java
+++ src/main/java/com/example/outsourcingprojects/common/config/WebMvcConfig.java
@@ -1,6 +1,8 @@
 package com.example.outsourcingprojects.common.config;
 
 import com.example.outsourcingprojects.common.aop.ActivityLoggingAspect;
+import com.example.outsourcingprojects.common.filter.HttpServletWrappingFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,7 +11,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Bean
-    public ActivityLoggingAspect activityLoggingAccept(){
+    public ActivityLoggingAspect activityLoggingAccept() {
         return new ActivityLoggingAspect();
+    }
+
+    @Bean
+    public FilterRegistrationBean<HttpServletWrappingFilter> firstFilterRegister() {
+        FilterRegistrationBean<HttpServletWrappingFilter> registrationBean =
+                new FilterRegistrationBean<>(new HttpServletWrappingFilter());
+        registrationBean.setOrder(Integer.MIN_VALUE);
+
+        return registrationBean;
     }
 }
--- src/main/java/com/example/outsourcingprojects/domain/task/controller/TaskController.java
+++ src/main/java/com/example/outsourcingprojects/domain/task/controller/TaskController.java
@@ -1,5 +1,6 @@
 package com.example.outsourcingprojects.domain.task.controller;
 
+import com.example.outsourcingprojects.common.aop.Loggable;
 import com.example.outsourcingprojects.domain.task.dto.CreateTaskRequestDto;
 import com.example.outsourcingprojects.domain.task.dto.CreateTaskResponseDto;
 import com.example.outsourcingprojects.domain.task.service.TaskService;
@@ -18,6 +19,7 @@ public class TaskController {
     private final TaskService taskService;
 
     // 작업 생성
+    @Loggable
     @PostMapping
     public ResponseEntity<CreateTaskResponseDto> createTask(
             @RequestBody CreateTaskRequestDto request
@@ -41,6 +43,7 @@ public class TaskController {
     }
 
     // 작업 수정
+    @Loggable
     @PutMapping("/{taskId}")
     public ResponseEntity<CreateTaskResponseDto> updateTask(
             @PathVariable Long taskId, @RequestBody CreateTaskRequestDto request, @RequestParam Long userId) {
