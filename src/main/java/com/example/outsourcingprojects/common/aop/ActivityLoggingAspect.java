337d090160ba6a9d7b643d1e17ce6d802d1ad551
윤지현 <139779783+jheony@users.noreply.github.com>
2025-12-12 18:24:36 +0900

    feat: 내 활동 로그 조회 구현
    - description 생성 로직 추가
    - QueryDSL 적용
    - PageDataDto 적용

--- src/main/java/com/example/outsourcingprojects/common/aop/ActivityLoggingAspect.java
+++ src/main/java/com/example/outsourcingprojects/common/aop/ActivityLoggingAspect.java
@@ -1,15 +1,17 @@
 package com.example.outsourcingprojects.common.aop;
 
 import com.example.outsourcingprojects.common.entity.ActivityLog;
+import com.example.outsourcingprojects.common.entity.Task;
 import com.example.outsourcingprojects.common.entity.User;
 import com.example.outsourcingprojects.common.model.ActivityType;
+import com.example.outsourcingprojects.common.model.TaskStatusType;
 import com.example.outsourcingprojects.common.util.response.GlobalResponse;
 import com.example.outsourcingprojects.domain.activitylog.repository.ActivityLogRepository;
+import com.example.outsourcingprojects.domain.task.repository.TaskRepository;
 import com.example.outsourcingprojects.domain.team.dto.response.CreateTeamResponseDto;
 import com.example.outsourcingprojects.domain.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
@@ -28,22 +30,23 @@ public class ActivityLoggingAspect {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private TaskRepository taskRepository;
+
     @Pointcut("@annotation(com.example.outsourcingprojects.common.aop.Loggable)")
     public void ActivityLoggingPointcut() {
     }
 
     @AfterReturning(value = "ActivityLoggingPointcut()", returning = "retVal")
-    public Object ActivityLoggingAdvice(JoinPoint joinPoint, Object retVal) throws Throwable {
+    public Object ActivityLoggingAdvice(Object retVal) {
 
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
         String requestURI = request.getRequestURI();
 
         Long userId = (Long) request.getAttribute("userId");
         User user = userRepository.findById(userId).orElseThrow(() -> new IllegalStateException("사용자 없음"));
-//        log.info("user = {}", user.getName());
 
         GlobalResponse result = (GlobalResponse) retVal;
-//        log.info("result = {}", result);
 
         String[] msg = result.getMessage().split(" ");
         String target = msg[0].substring(0, 2);
@@ -51,7 +54,6 @@ public class ActivityLoggingAspect {
         String action = target + " " + activity;
 
         LocalDateTime timestamp = result.getTimestamp();
-//        log.info("timestamp = {}", timestamp);
 
         String method = request.getMethod();
         String[] uri = requestURI.split("/");
@@ -62,18 +64,35 @@ public class ActivityLoggingAspect {
             taskId = Long.parseLong(uri[3]);
         }
 
+        // 작업 생성 시 작업 아이디 가져오기
         if (method.equals("POST") && target.equals("작업")) {
             CreateTeamResponseDto responseDto = (CreateTeamResponseDto) result.getData();
             taskId = responseDto.getId();
         }
 
-        Long activityType = ActivityType.methodToType(action, method).getActivityNum();
+        ActivityType activityType = ActivityType.methodToType(action, method);
+
+        Task task = taskRepository.findById(taskId).orElseThrow(() -> new IllegalStateException("작업 없음"));
+
+        String description = activityType.getFormatDescription(task.getTitle());
+
+        // 작업 상태 변경 시 description 설정
+        if (method.equals("PATCH")) {
+
+            TaskStatusType taskStatusType = TaskStatusType.toType(task.getStatus());
+
+            TaskStatusType[] taskStatusTypes = TaskStatusType.values();
+
+            int curStatus = (taskStatusTypes[1].equals(taskStatusType)) ? 0 : 1;
+
+            description = activityType.getFormatDescription(taskStatusTypes[curStatus].name(), taskStatusType.name());
+        }
 
         ActivityLog activityLog = new ActivityLog(
                 user,
                 taskId,
-                activityType,
-                "", // TODO description 생성? 메서드 구현?
+                activityType.getActivityNum(),
+                description,
                 timestamp
         );
 
